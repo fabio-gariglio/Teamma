7bae45687032cc5ec199a64cdc57417c5b1dece7
fabio-gariglio <garigliofabio@gmail.com>
2019-03-15 22:27:56 +0000

    Add MainController with visits counter

---
+++ src/FrontEnd/Controllers/MainController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontEnd.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class MainController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<object> Get()
+        {
+            return new
+            {
+                Counter = 0
+            };
+        }
+    }
+}
